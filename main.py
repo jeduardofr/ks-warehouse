33de8812635e1d581ce6770512f18ed7a93bcabc
AzzuEsau <andres.lopez4191@alumnos.udg.mx>
2021-05-06 09:37:50 -0500

    Finish Him

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import pandas as pd
 from sqlalchemy import create_engine, types
-from sqlalchemy.types import String, Integer, Date, TIMESTAMP, Float
+from sqlalchemy.types import String, Integer, Date, DATETIME, Float
 
 data = pd.read_csv('ks-projects/ks-projects-201801.csv')
 
 map_columns_to_types = {
     "ID": Integer,
-    "name": String,
-    "category": String,
-    "main_category": String,
-    "currency": String,
+    "name": String(200),
+    "category": String(200),
+    "main_category": String(200),
+    "currency": String(200),
     "deadline": Date,
     "goal": Float,
-    "launched": TIMESTAMP,
+    "launched": DATETIME,
     "pledged": Float,
-    "state": String,
+    "state": String(200),
     "backers": Integer,
-    "country": String,
+    "country": String(200),
     "usd pledged": Float,
     "usd_pledged_real": Float,
     "usd_goal_real": Float
 }
 
-engine = create_engine('mysql://sk-user:secret@db:3306/sk')
+engine = create_engine('mysql://root:@localhost:3306/ks')
+
+engine.execute("drop table projects")
 
 data.to_sql(
     "projects", # Nombre de la tabla
